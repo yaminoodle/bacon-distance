eb572813f60fad00e5a76918ff86006a852887f6
yaminoodle <binyaminwieder@gmail.com>
2026-03-17 17:00:09 +0200

    Restructure script as package.

---
+++ backend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bacon_dist_backend",
+    version="1.0",
+    description="A backend with an API to calculate 'Bacon Distance'.",
+    author="Binyamin Wieder",
+    packages=find_packages(),
+    install_requires=["polars"],
+)
