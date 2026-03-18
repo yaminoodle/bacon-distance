6d6ccfb6596a1056d173cfebf0ff3c97694e32f8
yaminoodle <binyaminwieder@gmail.com>
2026-03-18 12:07:56 +0200

    Implement bacon distance calculation api

--- backend/bacon_dist_backend/main.py
+++ backend/bacon_dist_backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from bacon_dist_backend.bacon_distance_calculator import BaconDistanceCalculator
+from bacon_dist_backend.data_frames_manager import DataFramesManager
 
+app = FastAPI()
+df_manager = DataFramesManager("../db.json")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/bacon-dist/{actor_name}")
+async def calc_bacon_distance(actor_name: str):
+
+    bacon_distance_calculator = BaconDistanceCalculator()
+    distance = bacon_distance_calculator.dist_from_actor(actor_name, df_manager)
+
+    return {"bacon_distance": distance.to_eng_string()}
